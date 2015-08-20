e190200584546764316b04dbd60a2eba16b0a754
giganoide <giganoide@hotmail.com>
2015-08-20 22:32:55 +0200

    Initial commit of the BowlingGameKata

---
+++ BowlingGame/BowlingGame/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private readonly int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            for (int i = 0; i < rolls.Length; i++)
+                score += rolls[i];
+            return score;
+        }
+    }
+}
---
+++ BowlingGame/BowlingGameTest/BowlingGameTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using BowlingGame;
+
+namespace BowlingGameTest
+{
+    [TestFixture]
+    public class BowlingGameTest
+    {
+        private Game g;
+
+        [SetUp]
+        public void CreateGame()
+        {
+            g = new Game();    
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+                g.Roll(pins);
+        }
+
+        [Test]
+        public void TestGutterGame()
+        {
+            RollMany(20, 0);
+            Assert.That(g.Score(), Is.EqualTo(0));
+        }
+        
+        [Test]
+        public void TestAllOnes()
+        {
+            RollMany(20,1);
+            Assert.That(g.Score(), Is.EqualTo(20));
+        }
+
+        [Test]
+        [Ignore]
+        public void TestOneSpare()
+        {
+            g.Roll(5);
+            g.Roll(5); // spare
+            g.Roll(3);
+            RollMany(17,0);
+            Assert.That(g.Score(), Is.EqualTo(16));
+        }
+
+    }
+}
