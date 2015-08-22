bffcb7ab2b3315ea5097e5dc974364fe173020da
giganoide <giganoide@hotmail.com>
2015-08-22 18:29:21 +0200

    Initial commit of FizzBuzzGame

---
+++ FizzBuzzGame/FizzBuzzGame/FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzGame
+{
+    public class FizzBuzz
+    {
+        public string GetString(int number)
+        {
+            string sReturn = String.Empty;
+            sReturn += GetFizz(number);
+            sReturn += GetBuzz(number);
+            return (!sReturn.Equals(string.Empty)) ? sReturn : number.ToString();
+
+        }
+
+        private string GetBuzz(int number)
+        {
+            return IsDivisibleBy(number, 5) ? "Buzz" : string.Empty;
+        }
+
+        private string GetFizz(int number)
+        {
+            return IsDivisibleBy(number, 3) ? "Fizz" : string.Empty;
+        }
+
+        private bool IsDivisibleBy(int number, int divisor)
+        {
+            return number % divisor == 0;
+        }
+    }
+}
---
+++ FizzBuzzGame/FizzBuzzGameTest/FizzBuzzGameTest.cs
@@ -0,0 +1,22 @@
+﻿using FizzBuzzGame;
+using NUnit.Framework;
+
+namespace FizzBuzzGameTest
+{
+    public class FizzBuzzGameTest
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30, "FizzBuzz")]
+        public void TestPositiveNumber(int number, string expected)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            Assert.That(expected, Is.EqualTo(fizzBuzz.GetString(number)));
+        }
+    }
+}
