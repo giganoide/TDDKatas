c97e4825c1140ed9d4fb1d3eda6503055a8690c6
giganoide <giganoide@hotmail.com>
2015-09-19 16:39:52 +0200

    Implemented algorithm

--- RomanNumerals/RomanNumeralConverter/RomanNumeralConverter.cs
+++ RomanNumerals/RomanNumeralConverter/RomanNumeralConverter.cs
@@ -1,16 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RomanNumeral
 {
     public class RomanNumeralConverter
     {
+        /*
+        Symbol 	Value
+            I 	1
+            V 	5
+            X 	10
+            L 	50
+            C 	100
+            D 	500
+            M 	1,000
+        */
+        static Dictionary<int, string> map = new Dictionary<int, string> { { 1, "I" }, { 5, "V" }, { 10, "X" }, { 50, "L" }, { 100, "C" }, { 500, "D" }, { 1000, "M" } };
+
         public static string Convert(int number)
         {
-            return "";
+            string romanNumber = string.Empty;
+
+            foreach (KeyValuePair<int, string> i in map.OrderByDescending(x => x.Key))
+            {
+                if (number >= i.Key)
+                {
+                    romanNumber += i.Value;
+                    number -= i.Key;
+                }
+            }
+            
+            for (int i = 0; i < number; i++)
+                romanNumber += "I";
+            return romanNumber;
         }
     }
 }
+;
--- RomanNumerals/RomanNumeralConverterTest/RomanNumeralConverterTest.cs
+++ RomanNumerals/RomanNumeralConverterTest/RomanNumeralConverterTest.cs
@@ -11,6 +11,14 @@ namespace RomanNumeralConverterTest
     public class RomanNumeralConverterTest
     {
         [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(5, "V")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(10, "X")]
+        [TestCase(13, "XIII")]
+        [TestCase(50, "L")]
         public void Convert_a_number_to_a_roman_numeral(int number, string expected)
         {
             Assert.That(expected, Is.EqualTo(RomanNumeralConverter.Convert(number)));
